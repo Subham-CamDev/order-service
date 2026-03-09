2fa7c6b9d6b61e4ea8860ea4489c7a54284c080b
Subham-CamDev <subhambhatta2212@gmail.com>
2026-03-09 21:52:31 +0530

    Added ObservationConfig class

--- src/main/java/com/subham/microservices/configuration/ObservationConfig.java
+++ src/main/java/com/subham/microservices/configuration/ObservationConfig.java
@@ -2,11 +2,9 @@ package com.subham.microservices.configuration;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 
 @Configuration
 @RequiredArgsConstructor
